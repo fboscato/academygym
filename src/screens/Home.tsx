3999f3c0ae284b36635c068c59b6deeb440f545c
fernandoboscato <fernandoboscato@faee.com.br>
2024-07-11 11:54:34 -0300

    inicio da aula finalizando interfaces

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,51 @@
-import { Center, Text } from "native-base";
+import { ExerciseCard } from "@components/ExerciseCard";
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
+import { useState } from "react";
 
-export function Home(){
-  return(
-    <Center flex={1}>
-      <Text color='white'>Home</Text>
-    </Center>
-  )  
+export function Home() {
+  const [group, setGroup] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro',])
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terra'])
+  const [groupSelected, setGroupSelected] = useState('Costas')
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+      <FlatList
+        data={group}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected.toUpperCase() ===  item.toUpperCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        maxH={10}
+        my={10}
+      />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>        
+        <FlatList
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsHorizontalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
+    </VStack>
+  )
 }
