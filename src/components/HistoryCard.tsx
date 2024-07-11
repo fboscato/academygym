f2215ca577d37fbe6ca528ce9dfb0b14427ec367
fernandoboscato <fernandoboscato@faee.com.br>
2024-07-11 14:52:29 -0300

    finalizado a aula finalizando interfaces

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -11,17 +11,17 @@ export function HistoryCard() {
       alignItems="center"
       justifyContent="space-between"
     >
-      <VStack mr={5}>
-        <Heading color="white" fontSize="md" textTransform="capitalize">
-          Costas
+      <VStack mr={5} flex={1}>
+        <Heading color="white" fontSize="md" textTransform="capitalize" numberOfLines={1}>
+          Costas 
         </Heading>
         <Text color="gray.100" fontSize="lg" numberOfLines={1}>
           Puxada frontal
         </Text>
+      </VStack>
         <Text color="gray.300" fontSize="md">
           08:56
         </Text>
-      </VStack>
     </HStack>
   )
 }
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,78 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+import { TouchableOpacity } from "react-native";
+import { UserPhoto } from "@components/UserPhoto";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Center, ScrollView, Text, VStack, Skeleton, Heading } from "native-base";
 
-export function Profile(){
-  return(
-    <Center flex={1}>
-    <Text color='white'>Profile</Text>
-    </Center>
-  )  
+const PHOTO_SIZE = 33
+
+export function Profile() {
+
+  const [photoIsLoading, setPhotoIsLoading] = useState()
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt={6} px={10}>
+          {photoIsLoading ?
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded='full'
+              startColor="gray.500"
+              endColor="gray.300"
+            />
+            :
+            <UserPhoto
+              source={{ uri: 'http://github.com/fboscato.png' }}
+              alt="Foto usuario"
+              size={PHOTO_SIZE}
+            />
+          }
+          <TouchableOpacity>
+            <Text color='green.500' fontWeight="bold" fontSize='md' mt='2' mb={8}>
+              Altera foto
+            </Text>
+          </TouchableOpacity>
+          <Input
+            bg='gray.600'
+            placeholder="Nome"
+          />
+          <Input
+            bg='gray.600'
+            placeholder="E-mail"
+            isDisabled
+          />
+
+          <Heading color="gray.200" fontSize="md" mb={2}
+            alignSelf='flex-start' mt={12}
+          >
+            ALtera senha
+          </Heading>
+          <Input
+            bg='gray.600'
+            placeholder="Senha antiga"
+            secureTextEntry
+          />
+          <Input
+            bg='gray.600'
+            placeholder="Nova senha"
+            secureTextEntry
+          />
+          <Input
+            bg='gray.600'
+            placeholder="Confirme nova senha "
+            secureTextEntry
+          />
+          <Button
+            title="Alterar"
+            mt={4}
+          />
+        </Center>
+      </ScrollView>
+    </VStack>
+  )
 }
