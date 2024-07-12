06e27945e91d569b63c6f592481f5a3835e81eae
fernandoboscato <fernandoboscato@faee.com.br>
2024-07-12 17:30:48 -0300

    alt

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,10 +4,16 @@ import LogoSvg from '../assets/logo.svg'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
+import {} from '@react-hook-form'
 
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const navigation = useNavigation();
-
+  // useForm
   function handleGoBack() {
     navigation.goBack();
   }
@@ -36,15 +42,23 @@ export function SignUp() {
           </Heading>
           <Input
             placeholder='Nome'
+            onChangeText={setName}
           />
           <Input
             placeholder='E-mail'
             keyboardType='email-address'
             autoCapitalize='none'
+            
           />
           <Input
             placeholder='Senha'
             secureTextEntry
+            
+          />
+           <Input
+            placeholder='Confirme a senha'
+            secureTextEntry
+            
           />
           <Button title='Criar e acessar' />
         </Center>
