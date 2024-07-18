26f574245008cacecc099bd7e22c4871a9d769fd
fernandoboscato <fernandoboscato@faee.com.br>
2024-07-18 18:20:33 -0300

    autencicação

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -5,15 +5,15 @@ import { useAuth } from "@hooks/useAuth";
 import { AppRoutes } from "./app.routes";
 export function Routes() {
   const { colors } = useTheme()
-  const {user} = useAuth()
-  console.log("Usuario logado:", user)
-  
-  const theme = DefaultTheme  
+  const { user } = useAuth()
+
+
+  const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
   return (
     <Box flex={1} bg={"gray.700"}>
       <NavigationContainer theme={theme}>
-        <AuthRouter />        
+        {user.id ? <AppRoutes /> : <AuthRouter />}
       </NavigationContainer>
     </Box>
   )
