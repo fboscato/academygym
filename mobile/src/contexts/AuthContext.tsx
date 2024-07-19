df589d9e36d202181fd244a760af540b2a827c08
fernandoboscato <fernandoboscato@faee.com.br>
2024-07-19 18:15:47 -0300

    alt

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,10 +1,12 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
+import { storageUserSave, storageUserGet } from "@storage/storageUser";
 import { AppErro } from "@utils/AppError";
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 export type AuthContextDataProps = {
   user: UserDTO
   singIn: (email: string, password: string) => Promise<void>
+  isLoadingUserStorageData: boolean
 }
 type AuthContextProviderProps = {
   children: ReactNode;
@@ -13,19 +15,39 @@ export const AuthContext = createContext<AuthContextDataProps>({} as AuthContext
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO)
+  const [isLoadingUserStorageData, setIsLoadingUserStorage] = useState(true)
+
   async function singIn(email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password })
       if (data.user) {
         setUser(data.user)
+        storageUserSave(data.user)
       }
-    } catch (error) {      
+    } catch (error) {
       throw error
     }
 
   }
+
+  async function loadUserData() {
+    try {
+      const userLogged = await storageUserGet()
+      if (userLogged) {
+        setUser(userLogged)
+        setIsLoadingUserStorage(false)
+      }
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorage(false)
+    }
+  }
+  useEffect(() => {
+    loadUserData()
+  }, [])
   return (
-    <AuthContext.Provider value={{ user, singIn }}>
+    <AuthContext.Provider value={{ user, singIn, isLoadingUserStorageData }}>
       {children}
     </AuthContext.Provider>
   )
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -3,13 +3,17 @@ import { AuthRouter } from "./auth.routes";
 import { useTheme, Box } from "native-base"
 import { useAuth } from "@hooks/useAuth";
 import { AppRoutes } from "./app.routes";
+import { Loading } from "@components/Loading";
 export function Routes() {
   const { colors } = useTheme()
-  const { user } = useAuth()
+  const { user,isLoadingUserStorageData } = useAuth()
 
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
+  if (isLoadingUserStorageData){
+    return<Loading/>
+  }
   return (
     <Box flex={1} bg={"gray.700"}>
       <NavigationContainer theme={theme}>
